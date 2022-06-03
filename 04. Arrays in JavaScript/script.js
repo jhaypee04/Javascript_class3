902c1b6dbecb6553d58298647c9ea622330195ad
EDOZIE JOHNPAUL UZOMA <pauledozie004@gmail.com>
2022-06-03 11:32:48 +0100

    Added break; to the switch statement

---
+++ 04. Arrays in JavaScript/script.js
@@ -0,0 +1,47 @@
+// alert(123);
+
+// Arrays
+// What is an Array?
+// Arrays stores multiple values in one variable
+// It is a data type that falls under the non-primitive
+// Syntax:
+var any_variable_Name = []; // An empty array
+ var arr = [ 1, "Kenny", 3, 4, true, 56, 7, 8]; // An array with values;
+
+//  console.log( arr[1] );  // Accessing the values from the arrays
+ 
+ // Overriding the values in am array
+ arr[1] = "Fola";
+
+//  console.log( arr[1] );
+
+ // Methods
+ // What is a method?
+ // Methods are functions!
+ // Examples of methods are 
+ // 1. length
+ // 2.
+
+ console.log( arr.length );
+
+ // Deleting values from arrays
+ // pop (method)
+ // shift (method)
+ console.log(arr);
+ arr.pop(); // Deletes the last value(item)
+ console.log(arr);
+ arr.shift(); // Deletes the first value (item)
+ console.log(arr);
+
+//  Adding values to arrays
+// push (method)
+
+arr.push("PrinceWill");
+console.log(arr);
+
+// Class Work 1
+// 1. Create an array to use as your shopping list with 3 items: "Milk," "Bread," and "Apples."
+// 2. Check your list length in the console.
+// 3. Update "Bread" to "Bananas."
+// 4. Output your entire list to the console.
+ 
